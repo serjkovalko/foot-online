7e4a17689a1fde8a46254ceb1d8377a21d198380
serjkovalko <serjkovalko@gmail.com>
2021-04-28 11:48:16 +0300

    finished project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-
-})
+  wow = new WOW(
+    {
+      boxClass: 'wow',      // default
+      animateClass: 'animate__animated', // default
+      offset: 0,          // default
+      mobile: true,       // default
+      live: true        // default
+    }
+  )
+  wow.init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ function scripts() {
      'node_modules/rateyo/src/jquery.rateyo.js',
      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+     'node_modules/wowjs/dist/wow.js',
       'app/js/main.js'
    ])
       .pipe(concat('main.min.js'))
